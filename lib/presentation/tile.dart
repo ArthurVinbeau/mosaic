4962161a546ca838885c47b970276336a17a1aa3
Théo GOLGEVIT <theo.golgevit@epita.fr>
2022-04-21 17:05:23 +0200

    feat(Tile): tile design

--- lib/presentation/tile.dart
+++ lib/presentation/tile.dart
@@ -1,12 +1,34 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mosaic/game_bloc.dart';
 
 class Tile extends StatelessWidget {
-  const Tile({Key? key}) : super(key: key);
+  final int i, j;
+
+  final String? label;
+  final bool? state;
+  final double size;
+
+  const Tile({Key? key, required this.i, required this.j, this.label, this.state, this.size = 50}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return InkWell(
+      onTap: () => context.read<GameBloc>().add(TilePressedGameEvent(i, j, false)),
+      onLongPress: () => context.read<GameBloc>().add(TilePressedGameEvent(i, j, true)),
+      child: Container(
+        height: size,
+        width: size,
+        color: state == null
+            ? Colors.teal
+            : state!
+                ? Colors.black
+                : Colors.white,
+        child: label != null
+            ? AutoSizeText(label!, style: TextStyle(color: (state ?? false) ? Colors.white : Colors.black))
+            : null,
+      ),
+    );
   }
-
 }
