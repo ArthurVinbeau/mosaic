4828d55e03d61a77627c1afa6f08fe138dc78075
Théo GOLGEVIT <theo.golgevit@epita.fr>
2022-04-21 19:12:52 +0200

    feat(Cell): cell error bool

--- lib/entities/cell.dart
+++ lib/entities/cell.dart
@@ -5,6 +5,14 @@ class Cell {
   bool full;
   bool empty;
   bool? state;
+  bool error;
 
-  Cell({this.clue = 0, this.shown = true, required this.value, this.full = false, this.empty = false, this.state});
+  Cell(
+      {this.clue = 0,
+      this.shown = true,
+      required this.value,
+      this.full = false,
+      this.empty = false,
+      this.state,
+      this.error = false});
 }
