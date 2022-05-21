6d61d397a209f37e52202652eaa4364e364495a2
Théo GOLGEVIT <theo.golgevit@epita.fr>
2022-05-21 14:34:04 +0200

    feat(GamePage): game controls position depends on screen aspect ratio

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mosaic/utils/themes.dart';
 
 import 'blocs/AppState/app_state_bloc.dart';
 import 'blocs/Game/game_bloc.dart';
@@ -16,8 +17,74 @@ class GamePage extends StatefulWidget {
 class _GamePageState extends State<GamePage> with WidgetsBindingObserver {
   late final AppStateBloc _bloc;
 
+  Widget _getControls(BuildContext context, bool vertical) {
+    final theme = baseTheme.dark;
+    return BlocBuilder<GameBloc, GameState>(
+      builder: (context, state) {
+        GameControls controls;
+        if (state is ControlsGameState) {
+          controls = state.controls;
+        } else if (state is NewBoardGameState) {
+          controls = state.controls;
+        } else {
+          return const SizedBox();
+        }
+        Color filled = theme.cellFilled, empty = theme.cellEmpty;
+
+        if (controls.reversed) {
+          var tmp = filled;
+          filled = empty;
+          empty = tmp;
+        }
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Wrap(
+            alignment: WrapAlignment.center,
+            direction: vertical ? Axis.vertical : Axis.horizontal,
+            spacing: 8.0,
+            children: [
+              IconButton(
+                  onPressed: () => context.read<GameBloc>().add(ToggleColorsEvent()),
+                  icon: Stack(
+                    children: [Icon(Icons.circle, color: empty), Icon(Icons.contrast, color: filled)],
+                  )),
+              Ink(
+                decoration: ShapeDecoration(shape: const CircleBorder(), color: controls.fill ? filled : null),
+                child: IconButton(
+                    onPressed: () => context.read<GameBloc>().add(ToggleFillEvent()),
+                    icon: Icon(Icons.format_color_fill, color: controls.fill ? empty : filled)),
+              ),
+              IconButton(
+                  onPressed: controls.canUndo ? () => context.read<GameBloc>().add(UndoEvent()) : null,
+                  icon: const Icon(Icons.undo),
+                  color: theme.controlsMoveEnabled,
+                  disabledColor: theme.controlsMoveDisabled),
+              IconButton(
+                  onPressed: controls.canRedo ? () => context.read<GameBloc>().add(RedoEvent()) : null,
+                  icon: const Icon(Icons.redo),
+                  color: theme.controlsMoveEnabled,
+                  disabledColor: theme.controlsMoveDisabled),
+            ],
+          ),
+        );
+      },
+      buildWhen: (_, b) => b is ControlsGameState || b is NewBoardGameState,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final theme = baseTheme.dark;
+
+    Widget body = const BoardWidget();
+
+    final size = MediaQuery.of(context).size;
+
+    final bool vertical = size.height <= size.width;
+
+    if (vertical) {
+      body = Row(children: [_getControls(context, true), SizedBox(width: size.width - 64, child: body)]);
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Mosaic"),
@@ -30,54 +97,9 @@ class _GamePageState extends State<GamePage> with WidgetsBindingObserver {
           )
         ],
       ),
-      body: const BoardWidget(),
-      backgroundColor: Colors.grey,
-      bottomNavigationBar: BlocBuilder<GameBloc, GameState>(
-        builder: (context, state) {
-          GameControls controls;
-          if (state is ControlsGameState) {
-            controls = state.controls;
-          } else if (state is NewBoardGameState) {
-            controls = state.controls;
-          } else {
-            return const SizedBox();
-          }
-          Color a = Colors.black, b = Colors.white;
-
-          if (controls.reversed) {
-            var tmp = a;
-            a = b;
-            b = tmp;
-          }
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              spacing: 8.0,
-              children: [
-                IconButton(
-                    onPressed: () => context.read<GameBloc>().add(ToggleColorsEvent()),
-                    icon: Stack(
-                      children: [Icon(Icons.circle, color: b), Icon(Icons.contrast, color: a)],
-                    )),
-                Ink(
-                  decoration: ShapeDecoration(shape: const CircleBorder(), color: controls.fill ? a : null),
-                  child: IconButton(
-                      onPressed: () => context.read<GameBloc>().add(ToggleFillEvent()),
-                      icon: Icon(Icons.format_color_fill, color: controls.fill ? b : a)),
-                ),
-                IconButton(
-                    onPressed: controls.canUndo ? () => context.read<GameBloc>().add(UndoEvent()) : null,
-                    icon: const Icon(Icons.undo)),
-                IconButton(
-                    onPressed: controls.canRedo ? () => context.read<GameBloc>().add(RedoEvent()) : null,
-                    icon: const Icon(Icons.redo)),
-              ],
-            ),
-          );
-        },
-        buildWhen: (_, b) => b is ControlsGameState || b is NewBoardGameState,
-      ),
+      body: body,
+      backgroundColor: theme.gameBackground,
+      bottomNavigationBar: vertical ? null : _getControls(context, false),
     );
   }
 
