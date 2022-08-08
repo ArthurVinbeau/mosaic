cc18ff6dc01d4aa3ee9749084fe138c2008e6425
Théo GOLGEVIT <theo.golgevit@epita.fr>
2022-08-08 20:03:40 +0200

    feat(Themes): Collection name & more collections

--- lib/utils/theme/themes.dart
+++ lib/utils/theme/themes.dart
@@ -3,20 +3,86 @@ import 'package:flutter/material.dart';
 class ThemeCollection {
   final GameTheme light;
   final GameTheme dark;
+  final String name;
 
-  const ThemeCollection({required this.light, required this.dark});
+  const ThemeCollection({required this.light, required this.dark, required this.name});
 
-  const ThemeCollection.unique(GameTheme theme)
+  const ThemeCollection.unique({required GameTheme theme, required this.name})
       : light = theme,
         dark = theme;
 }
 
+const List<ThemeCollection> themeCollections = [baseTheme, redTheme, blueTheme];
+
 const ThemeCollection baseTheme = ThemeCollection(
+    name: "Base",
     light: GameTheme(
         primaryColor: Colors.blue,
         menuBackground: Colors.white,
         gameBackground: Colors.grey,
         cellBase: Colors.teal,
+        cellEmpty: Colors.white,
+        cellFilled: Colors.black,
+        cellTextBase: Colors.black,
+        cellTextEmpty: Colors.black,
+        cellTextFilled: Colors.white,
+        cellTextError: Colors.red,
+        cellTextComplete: Colors.grey,
+        controlsMoveEnabled: Colors.black,
+        controlsMoveDisabled: Colors.black26),
+    dark: GameTheme(
+        primaryColor: Colors.red,
+        menuBackground: Color.fromARGB(255, 240, 240, 240),
+        gameBackground: Colors.black,
+        cellBase: Color.fromARGB(255, 148, 196, 190),
+        cellEmpty: Colors.white,
+        cellFilled: Color.fromARGB(255, 75, 75, 75),
+        cellTextBase: Colors.black,
+        cellTextEmpty: Colors.black,
+        cellTextFilled: Colors.white,
+        cellTextError: Colors.red,
+        cellTextComplete: Colors.grey,
+        controlsMoveEnabled: Colors.white,
+        controlsMoveDisabled: Color.fromARGB(255, 75, 75, 75)));
+
+const ThemeCollection redTheme = ThemeCollection(
+    name: "Red",
+    light: GameTheme(
+        primaryColor: Colors.blue,
+        menuBackground: Colors.white,
+        gameBackground: Colors.grey,
+        cellBase: Colors.pinkAccent,
+        cellEmpty: Colors.white,
+        cellFilled: Colors.black,
+        cellTextBase: Colors.black,
+        cellTextEmpty: Colors.black,
+        cellTextFilled: Colors.white,
+        cellTextError: Colors.red,
+        cellTextComplete: Colors.grey,
+        controlsMoveEnabled: Colors.black,
+        controlsMoveDisabled: Colors.black26),
+    dark: GameTheme(
+        primaryColor: Colors.red,
+        menuBackground: Color.fromARGB(255, 240, 240, 240),
+        gameBackground: Colors.black,
+        cellBase: Color.fromARGB(255, 148, 196, 190),
+        cellEmpty: Colors.white,
+        cellFilled: Color.fromARGB(255, 75, 75, 75),
+        cellTextBase: Colors.black,
+        cellTextEmpty: Colors.black,
+        cellTextFilled: Colors.white,
+        cellTextError: Colors.red,
+        cellTextComplete: Colors.grey,
+        controlsMoveEnabled: Colors.white,
+        controlsMoveDisabled: Color.fromARGB(255, 75, 75, 75)));
+
+const ThemeCollection blueTheme = ThemeCollection(
+    name: "Blue",
+    light: GameTheme(
+        primaryColor: Colors.blue,
+        menuBackground: Colors.white,
+        gameBackground: Colors.blueGrey,
+        cellBase: Colors.blueAccent,
         cellEmpty: Colors.white,
         cellFilled: Colors.black,
         cellTextBase: Colors.black,
