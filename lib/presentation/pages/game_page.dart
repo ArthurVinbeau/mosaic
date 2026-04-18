e19b5b6a39e0087f5f87554c434fb9a937ce81e7
Théo GOLGEVIT <theo.golgevit@epita.fr>
2026-04-18 12:31:03 +0200

    fix: fixed safe area

--- lib/presentation/pages/game_page.dart
+++ lib/presentation/pages/game_page.dart
@@ -22,74 +22,74 @@ class _GamePageState extends State<GamePage> with WidgetsBindingObserver {
 
   Widget _getControls(BuildContext context, GameTheme theme, bool vertical) {
     return RepaintBoundary(
-      child: BlocBuilder<GameBloc, GameState>(
-        builder: (context, state) {
-          GameControls controls;
-          if (state is ControlsGameState) {
-            controls = state.controls;
-          } else if (state is NewBoardGameState) {
-            controls = state.controls;
-          } else {
-            return const SizedBox();
-          }
-          Color filled = theme.cellFilled, empty = theme.cellEmpty;
+        child: BlocBuilder<GameBloc, GameState>(
+          builder: (context, state) {
+            GameControls controls;
+            if (state is ControlsGameState) {
+              controls = state.controls;
+            } else if (state is NewBoardGameState) {
+              controls = state.controls;
+            } else {
+              return const SizedBox();
+            }
+            Color filled = theme.cellFilled, empty = theme.cellEmpty;
 
-          if (controls.reversed) {
-            var tmp = filled;
-            filled = empty;
-            empty = tmp;
-          }
-          return Material(
-            color: theme.gameBackground,
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              width: vertical ? null : double.infinity,
-              height: vertical ? double.infinity : null,
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                direction: vertical ? Axis.vertical : Axis.horizontal,
-                spacing: 8.0,
-                children: [
-                  IconButton(
-                      onPressed: () =>
-                          context.read<GameBloc>().add(ToggleColorsEvent()),
-                      icon: Stack(
-                        children: [
-                          Icon(Icons.circle, color: empty),
-                          Icon(Icons.contrast, color: filled)
-                        ],
-                      )),
-                  Ink(
-                    decoration: ShapeDecoration(
-                        shape: const CircleBorder(),
-                        color: controls.fill ? filled : null),
-                    child: IconButton(
+            if (controls.reversed) {
+              var tmp = filled;
+              filled = empty;
+              empty = tmp;
+            }
+            return Material(
+              color: theme.gameBackground,
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                width: vertical ? null : double.infinity,
+                height: vertical ? double.infinity : null,
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  direction: vertical ? Axis.vertical : Axis.horizontal,
+                  spacing: 8.0,
+                  children: [
+                    IconButton(
                         onPressed: () =>
-                            context.read<GameBloc>().add(ToggleFillEvent()),
-                        icon: Icon(Icons.format_color_fill,
-                            color: controls.fill ? empty : filled)),
-                  ),
-                  IconButton(
-                      onPressed: controls.canUndo
-                          ? () => context.read<GameBloc>().add(UndoEvent())
-                          : null,
-                      icon: const Icon(Icons.undo),
-                      color: theme.controlsMoveEnabled,
-                      disabledColor: theme.controlsMoveDisabled),
-                  IconButton(
-                      onPressed: controls.canRedo
-                          ? () => context.read<GameBloc>().add(RedoEvent())
-                          : null,
-                      icon: const Icon(Icons.redo),
-                      color: theme.controlsMoveEnabled,
-                      disabledColor: theme.controlsMoveDisabled),
-                ],
+                            context.read<GameBloc>().add(ToggleColorsEvent()),
+                        icon: Stack(
+                          children: [
+                            Icon(Icons.circle, color: empty),
+                            Icon(Icons.contrast, color: filled)
+                          ],
+                        )),
+                    Ink(
+                      decoration: ShapeDecoration(
+                          shape: const CircleBorder(),
+                          color: controls.fill ? filled : null),
+                      child: IconButton(
+                          onPressed: () =>
+                              context.read<GameBloc>().add(ToggleFillEvent()),
+                          icon: Icon(Icons.format_color_fill,
+                              color: controls.fill ? empty : filled)),
+                    ),
+                    IconButton(
+                        onPressed: controls.canUndo
+                            ? () => context.read<GameBloc>().add(UndoEvent())
+                            : null,
+                        icon: const Icon(Icons.undo),
+                        color: theme.controlsMoveEnabled,
+                        disabledColor: theme.controlsMoveDisabled),
+                    IconButton(
+                        onPressed: controls.canRedo
+                            ? () => context.read<GameBloc>().add(RedoEvent())
+                            : null,
+                        icon: const Icon(Icons.redo),
+                        color: theme.controlsMoveEnabled,
+                        disabledColor: theme.controlsMoveDisabled),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-        buildWhen: (_, b) => b is ControlsGameState || b is NewBoardGameState,
-      ),
+            );
+          },
+          buildWhen: (_, b) => b is ControlsGameState || b is NewBoardGameState,
+        ),
     );
   }
 
@@ -97,18 +97,8 @@ class _GamePageState extends State<GamePage> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     return BlocBuilder<ThemeCubit, ThemeState>(
         builder: (BuildContext context, ThemeState state) {
-      Widget body = const RepaintBoundary(child: BoardWidget());
-
-      final size = MediaQuery.of(context).size;
-
-      final bool vertical = size.height <= size.width;
-
-      if (vertical) {
-        body = Row(children: [
-          SizedBox(width: size.width - 64, child: body),
-          _getControls(context, state.theme, true)
-        ]);
-      }
+      final size = MediaQuery.sizeOf(context);
+      final bool isLandscape = size.height <= size.width;
 
       final loc = AppLocalizations.of(context)!;
 
@@ -160,10 +150,15 @@ class _GamePageState extends State<GamePage> with WidgetsBindingObserver {
               ),
             ],
           ),
-          body: body,
+          body: SafeArea(child: Flex(
+            mainAxisSize: MainAxisSize.max,
+            direction: isLandscape ? Axis.horizontal : Axis.vertical,
+            children: [
+              Expanded(child: const RepaintBoundary(child: BoardWidget())),
+              _getControls(context, state.theme, isLandscape)
+            ],
+          )),
           backgroundColor: state.theme.gameBackground,
-          bottomSheet:
-              vertical ? null : _getControls(context, state.theme, false),
         ),
       );
     });
